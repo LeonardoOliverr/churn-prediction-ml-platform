c34d839be7ec715d93d9f9b37ebfc87231895de5
Leonardo Oliveira <ole071183@gmail.com>
2026-05-23 20:00:30 -0300

    feat: adiciona XGBoost como modelo de classificação de churn

--- ml/models/__init__.py
+++ ml/models/__init__.py
@@ -2,5 +2,6 @@
 
 from ml.models.baseline import SPECS as BASELINE_SPECS
 from ml.models.random_forest import SPECS as RANDOM_FOREST_SPECS
+from ml.models.xgboost import SPECS as XGBOOST_SPECS
 
-__all__ = ["BASELINE_SPECS", "RANDOM_FOREST_SPECS"]
+__all__ = ["BASELINE_SPECS", "RANDOM_FOREST_SPECS", "XGBOOST_SPECS"]
